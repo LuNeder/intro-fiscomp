c8b228bd692aa8275215df8e7ca6f88a9d81e994
Luana <luana@luana.dev.br>
2023-08-15 11:54:14 -0300

    t2

--- projeto-1/tarefa-2/tarefa-2-13836963.f
+++ projeto-1/tarefa-2/tarefa-2-13836963.f
@@ -2,6 +2,9 @@
       read(*, *) x1, y1, z1
       write(*, *) 'De x2, y2 e z2 de v2'
       read(*, *) x2, y2, z2
-      
-      
+      a1= y1*z2 - z1*y2
+      a2= z1*x2 - x1*z2
+      a3= x1*y2 - y1*x2
+      area = (sqrt((a1**2)+(a2**2)+(a3**2)))/2
+      write(*,*) 'A área do triângulo formado por v1 e v2 é', area
       end 
