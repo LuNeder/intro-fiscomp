7df9e0f3b93a49b5be91689647dc32d097286a31
Luana <luana@luana.dev.br>
2023-08-20 13:22:55 -0300

    ta indo

--- projeto-1/tarefa-3/tarefa-3-13836963.f
+++ projeto-1/tarefa-3/tarefa-3-13836963.f
@@ -1,11 +1,12 @@
       dimension a(20)
       write(*,*) 'De N e M, respectivamente, tal que o programa ira ler'
-      write(*,*) 'N numeros reais (em um arquivo) e ordenara os M primeiros menores numeros'
+      write(*,*) 'N numeros reais (em um arquivo) e ordenara '
+      write(*,*) 'os M primeiros menores numeros'
       read(*,*) n, m
-      open(unit=3, file='entrada-1-13836963')
-      read(3,*) a1,(a(j),j=1,19),aa1 
-      write(*,*) 'a1 =',a1 
-      write(*,*) 'vetor lido --->',(a(k),k=1,19)  
-      write(*,*) 'aa1 =',aa1 
-      close(3)  !fechou a unidade 3
+      open(unit=1, file='entrada-1-13836963')
+      read(1,*) (a(j),j=1,n)
+!      write(*,*) 'a1 =',a1 
+      write(*,*) 'vetor lido --->',(a(k),k=1,n)  
+!      write(*,*) 'aa1 =',aa1 
+      close(1)  
       end
