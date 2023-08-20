e2f3ea84cf086c5a0ad58887ce20d39e1d671167
Luana <luana@luana.dev.br>
2023-08-20 13:14:48 -0300

    cassilda, pq essa bosta nao compila

--- projeto-1/tarefa-3/tarefa-3-13836963.f
+++ projeto-1/tarefa-3/tarefa-3-13836963.f
@@ -1,4 +1,7 @@
       dimension a(20)
+      write(*,*) 'De N e M, respectivamente, tal que o programa ira ler'
+      write(*,*) 'N numeros reais (em um arquivo) e ordenara os M primeiros menores numeros'
+      read(*,*) n, m
       open(unit=3, file='entrada-1-13836963')
       read(3,*) a1,(a(j),j=1,19),aa1 
       write(*,*) 'a1 =',a1 
