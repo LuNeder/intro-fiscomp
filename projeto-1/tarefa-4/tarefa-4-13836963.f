1624396d9a03ddbe0d070900120bc11c4edbd276
Luana <luana@luana.dev.br>
2023-08-27 14:23:18 -0300

    cleaning

--- projeto-1/tarefa-4/tarefa-4-13836963.f
+++ projeto-1/tarefa-4/tarefa-4-13836963.f
@@ -1,4 +1,4 @@
-        real, dimension(:), allocatable :: a
+        integer, dimension(:), allocatable :: a
         
         write(*,*) 'De o numero N para ver se ele'
         write(*,*) 'e os numeros inferiores a ele sao primos'
@@ -11,30 +11,19 @@
         do while (i .GE. 1)
           j = n
           idivs=0
-        !  write(*,*) i
           do while (j .GE. 1)
             if (mod(i, j) .eq. 0) then
-              
               idivs = idivs+1
-            !  write(*,*) i, j, idivs
-              
-            
             end if
             j=j-1
           end do
           if (idivs .eq. 2) then
-         ! write(*,*) 'aqui'
             a(k) = i
             k = k+1
           end if
           i=i-1
         end do
-
-        write(*,*) a(1:k-1)
+      open(unit=2, file='saida-1-13836963')
+      write(2,*) a(1:k-1)
              
-
-
-
-          
-        
-        end
+      end
