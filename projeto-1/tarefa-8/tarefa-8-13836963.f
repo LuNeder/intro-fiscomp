460dbcf68826622babd2e7f1c78984016567ee92
Luana <luana@luana.dev.br>
2023-08-28 19:40:17 -0300

    arrumou

--- projeto-1/tarefa-8/tarefa-8-13836963.f
+++ projeto-1/tarefa-8/tarefa-8-13836963.f
@@ -6,7 +6,8 @@
 
       do idim = 2,d
         volume = 0
-        volume = ((pi**(idim/2))/gamma((idim/2)+1.0))*(R**idim)
+        exp = real(idim)
+        volume = ((pi**(exp/2))/gamma((exp/2)+1.0))*(R**exp)
         write(2,*) idim, volume
       end do
       
