0d31c387a96d561370a9defa4b971b5c405302fc
Luana <luana@luana.dev.br>
2023-08-29 20:56:29 -0300

    a

--- projeto-1/tarefa-5/tarefa-5-13836963.f
+++ projeto-1/tarefa-5/tarefa-5-13836963.f
@@ -1,14 +1,14 @@
       real eprec
       write(*,*) 'De x para calcular ln(x)'
-      read(*,*) x
+      read(*,*) y
+      x = y
       eprec = 0.00001
       pr = 1
       exp = 1
       aln = 0
-      e = 2.7182818285
       a = -1
+
       if (x .GE. 1) then
-        write(*,*) 'aqui'
         x = 1/x
         a = 1
       end if
@@ -17,13 +17,12 @@
       do while (pr .GE. eprec) 
         pr = (((1-x)**exp)/exp)
         aln = aln + (((1-x)**exp)/exp)
-        write(*,*) (((1-x)**exp)/exp), aln
         exp = exp+1
-
       end do
 
 
       aln = a* aln
-      write(*,*) aln
+      write(*,*) 'ln(x) = ', aln
+      write(*,*) 'interno:', log(y)
       
       end
