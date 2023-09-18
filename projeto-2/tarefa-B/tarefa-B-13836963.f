60c436845ead921e3ecaddde3ea9d858ddb09041
Luana <luana@luana.dev.br>
2023-09-18 11:38:55 -0300

    a

--- projeto-2/tarefa-B/tarefa-B-13836963.f
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -1,11 +1,19 @@
+        dimension a(1000)
         M = 1000 ! num de andarilhos
         N = 1000 ! num de passos
         p = 0.5  ! prob dir
         q = 1-p ! prob esq
-
-        do i=1,N
-          ir = rand()/p
-          
+        ipe = 0 !num passos esq
+        ipd = 0 ! num passos dir
+        itot = 0 ! total de passos
+        do j=1,M
+          do i=1,N
+            ir = rand()/p ! 0 = dir 1 = esq
+            ipe = ipe + ir
+            itot = itot+1
+          end do
+          ipd = itot - ipe
+          a(j) = ipd - ipe
         end do
         end
 
