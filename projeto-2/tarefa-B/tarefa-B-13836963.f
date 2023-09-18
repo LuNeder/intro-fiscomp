0b35590a15dbbff49efeb6c1071f40e6959fd2a6
Luana <luana@luana.dev.br>
2023-09-18 11:19:39 -0300

    a

---
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -0,0 +1,19 @@
+        M = 1000 ! num de andarilhos
+        N = 1000 ! num de passos
+        p = 0.5  ! prob dir
+        q = 1-p ! prob esq
+
+        do i=1,N
+          ir = rand()/p
+          
+        end do
+        end
+
+
+
+
+
+
+
+
+
