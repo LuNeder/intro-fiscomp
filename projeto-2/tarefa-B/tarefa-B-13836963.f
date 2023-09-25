fe7df8a10365f5717966a24a8116f3af4dbd23c2
Luana <luana@luana.dev.br>
2023-09-25 10:26:33 -0300

    sos

--- projeto-2/tarefa-B/tarefa-B-13836963.f
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -1,36 +1,38 @@
         dimension a(-1000:1000)
-        dimension b(0:2)
+        dimension b(0:1)
         a(-1000:1000) = 0
         b(0) = -1
-        b(1:2) = 1
+        b(1) = 1
         M = 1000 ! num de andarilhos
         N = 1000 ! num de passos
         ap = 0.5  ! prob dir
-!        q = 1-p ! prob esq
-!        ipe = 0 !num passos esq
-!        ipd = 0 ! num passos dir
+C       aq = 1-ap ! prob esq
+C        ipe = 0 !num passos esq
+C        ipd = 0 ! num passos dir
         itot = 0 ! total de passos
         iposx = 0 ! posicao
         open(unit=1, file='saida-1')
-        write(*,*) '1'
+C        write(*,*) '1'
         do j=1,M
-          write(*,*) '2'
+C          write(*,*) '2'
           do i=1,N
-            write(*,*) '2.5'
-            ir = rand()/ap ! 0 = dir 1 = esq
+C            write(*,*) '2.5'
+            ir = rand()/ap ! 0 = dir 1 = esq.
+            write(*,*) ir
             iposx = iposx + b(ir)
-!            itot = itot+1 
-            write(*,*) '3'           
+C            write(*,*) 'iposx = ', iposx
+C            itot = itot+1 
+C            write(*,*) '3'           
           end do
-          write(*,*) '4'
-          ! a(j) = iposx
-          write(*,*) 'iposx = ', iposx
+C          write(*,*) '4'
+C          a(j) = iposx
+          write(*,*) 'iposx-final = ', iposx
           a(iposx) = a(iposx) + 1
-          write(*,*) '5'
+C          write(*,*) '5'
         end do
-        write(*,*) '6'
-        do k = -N, N
-          write(1,*) k, a(k) 
+C        write(*,*) '6'
+        do k = 0-N, N
+          write(1,*) k, a(k)
         end do
         close(1)
         end
