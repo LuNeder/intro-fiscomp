d0db06ac793f95ec18662a24a5476087537b2acf
Luana <luana@luana.dev.br>
2023-09-25 09:57:24 -0300

    se eu tiro os write de debug n vai mais ??????????????????????????????????

--- projeto-2/tarefa-B/tarefa-B-13836963.f
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -1,26 +1,35 @@
         dimension a(-1000:1000)
         dimension b(0:200)
+        a(-1000:1000) = 0
         b(0) = -1
         b(1:200) = 1
-        M = 100000 ! num de andarilhos
+        M = 1000 ! num de andarilhos
         N = 1000 ! num de passos
-        p = 0.5  ! prob dir
-        q = 1-p ! prob esq
+        ap = 0.5  ! prob dir
+!        q = 1-p ! prob esq
 !        ipe = 0 !num passos esq
 !        ipd = 0 ! num passos dir
         itot = 0 ! total de passos
         iposx = 0 ! posicao
-        open(file='entrada-1', unit=1)
+        open(unit=1, file='saida-1')
+        write(*,*) '1'
         do j=1,M
+          write(*,*) '2'
           do i=1,N
-            ir = rand()/p ! 0 = dir 1 = esq
+            write(*,*) '2.5'
+            ir = rand()/ap ! 0 = dir 1 = esq
             iposx = iposx + b(ir)
-            itot = itot+1            
+!            itot = itot+1 
+            write(*,*) '3'           
           end do
+          write(*,*) '4'
           ! a(j) = iposx
+          write(*,*) 'iposx = ', iposx
           a(iposx) = a(iposx) + 1
+          write(*,*) '5'
         end do
-        do k = 0-M, M
+        write(*,*) '6'
+        do k = -N, N
           write(1,*) k, a(k) 
         end do
         close(1)
