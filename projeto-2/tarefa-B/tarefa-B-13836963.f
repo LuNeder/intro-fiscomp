684c5bdd9948e2ac06fbe8d36605178cc6dfe56c
Luana <luana@luana.dev.br>
2023-09-25 09:39:09 -0300

    sera q vai?

--- projeto-2/tarefa-B/tarefa-B-13836963.f
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -1,7 +1,7 @@
-        dimension a(100000)
+        dimension a(-1000:1000)
         dimension b(0:200)
         b(0) = -1
-        b(1:200) = 
+        b(1:200) = 1
         M = 100000 ! num de andarilhos
         N = 1000 ! num de passos
         p = 0.5  ! prob dir
@@ -10,14 +10,20 @@
      !   ipd = 0 ! num passos dir
         itot = 0 ! total de passos
         iposx = 0 ! posicao
+        open(file='entrada-1', unit=1)
         do j=1,M
           do i=1,N
             ir = rand()/p ! 0 = dir 1 = esq
             iposx = iposx + b(ir)
-            itot = itot+1
+            itot = itot+1            
           end do
-          a(j) = iposx
+          ! a(j) = iposx
+          a(iposx) = a(iposx) + 1
         end do
+        do k = 0-M, M
+          write(1,*) k, a(k) 
+        end do
+        close(1)
         end
 
 
