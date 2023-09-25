b05bc0c609f539942184cdde1033084feb6f4ae9
Luana <luana@luana.dev.br>
2023-09-25 10:50:57 -0300

    Alcaraz eh incrivel

--- projeto-2/tarefa-B/tarefa-B-13836963.f
+++ projeto-2/tarefa-B/tarefa-B-13836963.f
@@ -1,34 +1,30 @@
-        dimension a(-1000:1000)
-        dimension b(0:1)
-        a(-1000:1000) = 0
-        b(0) = -1
-        b(1) = 1
-        M = 1000 ! num de andarilhos
+        dimension ia(-1001:1001)
+        dimension ib(0:1)
+        ia(-1001:1001) = 0
+        ib(0) = 1
+        ib(1) = -1
+        M = 100000 ! num de andarilhos
         N = 1000 ! num de passos
-        ap = 0.5  ! prob dir
+        ap = 0.5e0  ! prob dir
         itot = 0 ! total de passos
         iposx = 0 ! posicao
         open(unit=1, file='saida-1')
         do j=1,M
+          iposx = 0
           do i=1,N
             ir = rand()/ap ! 0 = dir 1 = esq.
 C            write(*,*) ir
-            iposx = iposx + b(ir)           
+            iposx = iposx + ib(ir)           
           end do
 C          write(*,*) 'iposx-final = ', iposx
-          a(iposx) = a(iposx) + 1
+          ia(iposx) = ia(iposx) + 1
         end do
         do k = 0-N, N
-          write(1,*) k, a(k)
+          write(1,*) k, ia(k)
         end do
         close(1)
         end
-
-
-
-
-
-
-
-
-
+        
+        
+        
+        
