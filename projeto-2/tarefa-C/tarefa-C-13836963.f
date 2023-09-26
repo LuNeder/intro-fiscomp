f82ee1a85e10a90f791187ab90a5f9b88bd63080
Luana <luana@luana.dev.br>
2023-09-26 20:16:10 -0300

    c

--- projeto-2/tarefa-C/tarefa-C-13836963.f
+++ projeto-2/tarefa-C/tarefa-C-13836963.f
@@ -2,9 +2,12 @@
         ia(-1001:1001, -1001:1001) = 0
         M = 100000 ! num de andarilhos
         N = 10000 ! num de passos
-        ap = 0.25e0  ! prob cada - probs iguais
+        ! ap = 0.25e0  ! prob cada - probs iguais
         iposx = 0 ! posicao x
         iposy = 0 ! posicao y
+        avetr = 0 ! magnetude do vetor posicao r
+        sumr = 0
+        sumr2 = 0
         open(unit=1, file='saida-1')
         do j=1,M
           iposx = 0
@@ -26,8 +29,16 @@
             end if 
           end do
           write (1, *) iposx, iposy
+          r2 = iposx**2 + iposy**2
+          sx = sx + iposx
+          sy = sy + iposy
+          sumr2 = sumr2+s
+          sumr = sumr + sqrt(r2)
         end do
+
         close(1)
+        write(*,*) '<|r|> = ', (sumr/M)
+        write(*,*) 'del² = ', ((sumr2 -((sx**2) + (sy**2)))/M)
         end
         
         
