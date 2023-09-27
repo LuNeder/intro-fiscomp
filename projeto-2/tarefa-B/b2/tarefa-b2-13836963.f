198d10c645600be2ff4dd53e4a03f38cbb8c51d9
Luana <luana@luana.dev.br>
2023-09-27 11:55:11 -0300

    a

--- projeto-2/tarefa-B/b2/tarefa-b2-13836963.f
+++ projeto-2/tarefa-B/b2/tarefa-b2-13836963.f
@@ -5,7 +5,7 @@
         ib(1:100) = -1
         M = 100000 ! num de andarilhos
         N = 1000 ! num de passos
-        ap = 0.25e0  ! prob dir
+        ap = 0.20e0  ! prob dir - Alternar entre 1/3, 1/4 e 1/5
         itot = 0 ! total de passos
         iposx = 0 ! posicao
         open(unit=1, file='saida-1')
@@ -13,10 +13,8 @@
           iposx = 0
           do i=1,N
             ir = rand()/ap ! 0 = dir 1 = esq.
-C            write(*,*) ir
             iposx = iposx + ib(ir)           
           end do
-C          write(*,*) 'iposx-final = ', iposx
           ia(iposx) = ia(iposx) + 1
         end do
         do k = 0-N, N
