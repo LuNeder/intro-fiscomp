e96e7380f22e474cb71a300c2c4265dd18ca0864
Luana <luana@luana.dev.br>
2023-10-16 09:51:31 -0300

    a

--- projeto-3/tarefa-A/tarefa-A-13836963.f
+++ projeto-3/tarefa-A/tarefa-A-13836963.f
@@ -1,11 +1,31 @@
-      implicit real*8 (a-h)
+      program
+      implicit real*8 (a-g)
+      dimension h(14)
       dext1 = 9.796782013838 ! Derivada exata
       dext2 = 64.098324549472 ! Segunda derivada exata
       dext3 = 671.514613457866 ! Terceira derivada exata
+      h(1) = 0.5
+      h(2) = 0.2
+      h(3) = 0.1
+      h(4) = 0.05
+      h(5) = 0.01
+      h(6) = 0.005
+      h(7) = 0.001
+      h(8) = 0.0005
+      h(9) = 0.0001
+      h(10) = 0.00005
+      h(11) = 0.00001
+      h(12) = 0.000001
+      h(13) = 0.0000001
+      h(14) = 0.00000001
       
       
       
       
       
       
-      end      
+      
+      
+      end program      
+      
+      
