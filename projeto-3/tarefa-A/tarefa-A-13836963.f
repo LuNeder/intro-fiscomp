159ee7787eed46c5539b2a648c2c0f0311c3432a
Luana <luana@luana.dev.br>
2023-10-16 09:38:29 -0300

    a

--- projeto-3/tarefa-A/tarefa-A-13836963.f
+++ projeto-3/tarefa-A/tarefa-A-13836963.f
@@ -1,2 +1,11 @@
       implicit real*8 (a-h)
-       
+      dext1 = 9.796782013838 ! Derivada exata
+      dext2 = 64.098324549472 ! Segunda derivada exata
+      dext3 = 671.514613457866 ! Terceira derivada exata
+      
+      
+      
+      
+      
+      
+      end      
