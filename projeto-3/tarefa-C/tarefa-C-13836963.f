9ab183657aae906b14c60b5f9512d8ffe635966f
Luana <luana@luana.dev.br>
2023-10-22 18:32:37 -0300

    Update tarefa-C-13836963.f

---
+++ projeto-3/tarefa-C/tarefa-C-13836963.f
@@ -0,0 +1,26 @@
+       program c
+       implicit real*8 (a-h, o-z)
+       r1e = -7
+       r2e = 2
+       r3e = 9
+
+
+       end program
+
+
+
+       real*8 function f(x)
+         implicit real*8 (a-h, o-z)
+         f = (x**3)-(4*(x**2))-(59*x)+126
+       end function
+
+
+       real*8 function buscadireta()
+         implicit real*8 (a-h, o-z)
+         x = -10
+         f = 0
+         do while (x.lt.)
+
+         end do
+
+        end function
