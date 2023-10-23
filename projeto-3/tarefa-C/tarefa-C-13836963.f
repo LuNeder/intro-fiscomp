1f9b561ff05ce967db092c9ac574cae5ca45d910
Luana <luana@luana.dev.br>
2023-10-23 10:36:05 -0300

    famosa tecnica

--- projeto-3/tarefa-C/tarefa-C-13836963.f
+++ projeto-3/tarefa-C/tarefa-C-13836963.f
@@ -7,13 +7,15 @@
        x0 = -10.0d0
        
        xi1 = x0
+       xi2 = -0
+       xi3 = 7
 
        do i=0,6
          
-         ai = i
-         write (*,*) i
+          	 	
+        ! write (*,*) i
          tmp = buscadireta(x0)
-         
+        ! write (*,*) i
          open(unit=9, file="bd.tmp")
          read(9,*) bd1, bd2, bd3
          close(9)
@@ -21,9 +23,11 @@
          
          
          xi1 = anr(xi1)
+         xi2 = anr(xi2)
+         xi3 = anr(xi3)
 
-         write (*,4) ai, bd1, bd2, bd3, xi1
-4       format( 10('|', f20.10), '|')
+         write (*,4) i, bd1, bd2, bd3, xi1, xi2, xi3
+4       format( i2, 9('|', f20.10), '|')
 
        end do
 
@@ -48,35 +52,40 @@
 
        real*8 function buscadireta(x)
          implicit real*8 (a-h, o-z)
+       !  write(*,*) "bd"
          open(unit=9, file="bd.tmp")
+       !  write(*,*) "bdab"
+         x = -10.0d0
          f = -10.0d0
          do while (f.lt.0)
            f = fx(x)
            x = x + 0.1d0    
          end do
-         r1 = x - 0.1d0
+         r1 = x - 0.2d0
          f=10.0d0
 
          do while (f.gt.0)
            f = fx(x)
            x = x + 0.1d0    
          end do
-         r2 = x - 0.1d0
+         r2 = x - 0.2d0
          f=-10.0d0
 
          do while (f.lt.0)
            f = fx(x)
            x = x + 0.1d0    
          end do
-         r3 = x - 0.1d0
+         r3 = x - 0.2d0
 
          write(9,*) r1, r2, r3
+       !  write(*,*) "bdac"
          close(9)
+       !  write(*,*) "bdfc"
         end function
         
         real*8 function anr(x0)
           implicit real*8 (a-h, o-z)
-          write(*,*) "aqui"
+       !   write(*,*) "aqui"
           x = x0
           anr = x - (fx(x)/dfx(x))
           
