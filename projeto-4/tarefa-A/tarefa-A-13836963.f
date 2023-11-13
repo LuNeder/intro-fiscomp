4eb0cb8b2c6960b34fa676ba9c055bd80e8437a6
Luana <luana@luana.dev.br>
2023-11-13 10:38:14 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ projeto-4/tarefa-A/tarefa-A-13836963.f
@@ -0,0 +1,20 @@
+       program a
+       implicit real*8 (a-h, o-z)
+       g = 9.8d0
+       al = 9.8d0
+       am = 1.0d0
+       
+       
+       end program
+       
+       real*8 function omega()
+         implicit real*8 (a-h, o-z)
+         
+         
+       end function
+       
+       real*8 function theta()
+         implicit real*8 (a-h, o-z)
+         
+         
+       end function
