54cc8cad0ed7aad28dc6759217909683dae5999d
Luana <luana@luana.dev.br>
2023-11-18 19:32:25 -0300

    Update tarefa-A-13836963.f

--- projeto-4/tarefa-A/tarefa-A-13836963.f
+++ projeto-4/tarefa-A/tarefa-A-13836963.f
@@ -3,7 +3,29 @@
        g = 9.8d0
        al = 9.8d0
        am = 1.0d0
-       
+
+       tmax = 80.0d0
+       t = 0.0d0
+       dt = 0.01d0
+
+       omega = 0.0d0
+       theta = 0.0d0
+       pi = 4.0d0*datan(1.0d0)
+
+       do while (t.lt.tmax)
+         t = t + dt
+         thetz = theta
+         theta = theta + (omega * dt)
+         omega = omega - ((g/al)*thetz*dt)
+         e =  l * am * ( (((omega**2)*l)/2.0d0) + (g*(1-dcos(theta))) )
+
+        if(abs(theta).ge.2.0d0*pi) then
+          write(7,*)t,mod(theta,2.0d0*pi)
+        else
+          write(7,*)t,theta
+        end if
+
+       end do
        
        end program
        
