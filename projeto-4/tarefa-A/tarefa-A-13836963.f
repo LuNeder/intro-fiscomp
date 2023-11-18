77de180922fb33f21d05e7cac626eaf5a1f000c7
Luana <luana@luana.dev.br>
2023-11-18 19:39:53 -0300

    Update tarefa-A-13836963.f

--- projeto-4/tarefa-A/tarefa-A-13836963.f
+++ projeto-4/tarefa-A/tarefa-A-13836963.f
@@ -12,6 +12,9 @@
        theta = 0.0d0
        pi = 4.0d0*datan(1.0d0)
 
+       open(unit=7, file="theta")
+       open(unit=9, file="energia")
+
        do while (t.lt.tmax)
          t = t + dt
          thetz = theta
@@ -19,13 +22,18 @@
          omega = omega - ((g/al)*thetz*dt)
          e =  l * am * ( (((omega**2)*l)/2.0d0) + (g*(1-dcos(theta))) )
 
-        if(abs(theta).ge.2.0d0*pi) then
-          write(7,*)t,mod(theta,2.0d0*pi)
-        else
-          write(7,*)t,theta
-        end if
+         if(abs(theta).ge.2.0d0*pi) then
+           write(7,*) t,mod(theta,2.0d0*pi)
+         else
+           write(7,*) t,theta
+         end if
+
+         write(9,*) t, e
 
        end do
+
+       close(9)
+       close(7)
        
        end program
        
