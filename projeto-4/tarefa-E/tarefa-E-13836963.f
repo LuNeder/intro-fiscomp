af397975f85bc00895deeb6f3eca34fd8480f455
Luana <luana@luana.dev.br>
2023-11-21 12:19:15 -0300

    Update tarefa-E-13836963.f

---
+++ projeto-4/tarefa-E/tarefa-E-13836963.f
@@ -0,0 +1,41 @@
+       implicit real*8 (a-h, o-z)
+       g = 9.8d0
+       al = 9.8d0
+       am = 1.0d0
+
+       tmax = 80.0d0
+       t = 0.0d0
+       dt = 0.04d0
+
+       pi = 4.0d0*datan(1.0d0)
+       omega = 0.0d0
+       theta = pi/6.0d0
+       gamma = 0.05d0
+       OMEGAM = 2.0d0/3.0d0  
+
+       omega2 = 0.0d0
+       theta2 = (pi/6.0d0) +0.001d0
+     
+       F0 = 1.2d0
+       
+       open(unit=7, file="omega-12")
+       open(unit=9, file="omega2-12")
+
+       do while (t.lt.tmax)
+         t = t + dt
+         theta = theta + (omega * dt)
+         omega = omega - (g/al)*dsin(theta)*dt - gamma*omega*dt + F0
+     &*dsin(OMEGAM*t)*dt
+
+         theta2 = theta2 + (omega2 * dt)
+         omega2 = omega2 - (g/al)*dsin(theta2)*dt - gamma*omega2*dt + F0
+     &*dsin(OMEGAM*t)*dt
+
+         write(7,*) theta, omega
+         write(9,*) theta2, omega2
+       end do
+
+       close(7)
+       close(9)
+       end
+       
