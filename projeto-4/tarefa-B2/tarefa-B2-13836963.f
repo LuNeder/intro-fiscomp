413fb9926efd498067a0ca46c441bc8e288ac458
Luana <luana@luana.dev.br>
2023-11-21 10:45:16 -0300

    b

--- projeto-4/tarefa-B2/tarefa-B2-13836963.f
+++ projeto-4/tarefa-B2/tarefa-B2-13836963.f
@@ -7,27 +7,10 @@
       dt = 0.04d0
       pi = 4.0d0*datan(1.0d0)
 
-      open(unit=7,file="integral-periodo")
+      open(unit=7,file="pequeno-periodo")
       do i=1,30
         theta = 0.1d0*i
-        thetz = theta
-        omega = 0.0d0
-
-        t = 0.0d0
-
-        erro = thetz*0.01d0
-        h = 1/10000.0d0
-        hi = h
-
-        do while(h.le.(thetz-erro))
-          valor = b(h-hi,thetz,hi)
-          t = t + valor
-          h = h + 4*hi
-        end do
-
-        t = 2.0d0*dsqrt(2.0d0*al/g)*(t+2.0d0*
-     &dsqrt(erro/dsin(thetz)))
-        write(7,*)Periodo(g,al,pi,thetz),thetz
+        write(7,*)Periodo(g,al,pi,theta),theta
       end do
 
       close(7)
@@ -36,6 +19,6 @@
 
       real*8 function Periodo(g,al,pi,theta0)
         implicit real*8 (a-h,o-z)
-        T=2.0d0*pi*sqrt(al/g)*(1.0d0+((theta0**2.0d0)/16.0d0))
+        Periodo=(2.0d0*pi*sqrt(al/g)*(1.0d0+((theta0**2.0d0)/16.0d0)))
       end function
 
