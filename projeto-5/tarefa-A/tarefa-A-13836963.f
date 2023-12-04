9092641d4c493b1e4b03cf4c8fc33f80a7bc6ae3
Luana <luana@luana.dev.br>
2023-12-04 10:23:40 -0300

    a

--- projeto-5/tarefa-A/tarefa-A-13836963.f
+++ projeto-5/tarefa-A/tarefa-A-13836963.f
@@ -1,19 +1,23 @@
        program palavra
        implicit real*8 (a-h, o-z)
-       common x, y, xant, yant
-       m = 0
+       common x, y, xant, yant, GMs
+       pi = 4.0d0 * datan(1.0d0)
+       GMs = 4.0d0 * (pi**2)
+       r = sqrt((x**2.0d0)+(y**2.0d0))**3.0d0
+       dt = 1.0d0
 
        end program
 
        real*8 function xi(m)
          implicit real*8 (a-h, o-z)
-         common x, y, xant, yant
-         x = (2*x)-xant+()
+         common x, y, xant, yant, GMs
+         
+         x = (2.0d0*x)-xant+((-1.0d0)*((GMs*x)/(r))*(dt**2.0d0))
        end function
 
        real*8 function yi(m)
          implicit real*8 (a-h, o-z)
-         common x, y, xant, yant
+         common x, y, xant, yant, GMs
 
        end function
 
