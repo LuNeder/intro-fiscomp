75d730903126595691ecd8a32d70b99c924719ac
Luana <luana@luana.dev.br>
2023-12-04 10:34:40 -0300

    aaaa

--- projeto-5/tarefa-A/tarefa-A-13836963.f
+++ projeto-5/tarefa-A/tarefa-A-13836963.f
@@ -1,23 +1,26 @@
-       program palavra
        implicit real*8 (a-h, o-z)
        common x, y, xant, yant, GMs
        pi = 4.0d0 * datan(1.0d0)
        GMs = 4.0d0 * (pi**2)
-       r = sqrt((x**2.0d0)+(y**2.0d0))**3.0d0
-       dt = 1.0d0
-
-       end program
-
-       real*8 function xi(m)
-         implicit real*8 (a-h, o-z)
-         common x, y, xant, yant, GMs
-         
+       dt = 0.04d0
+       tf = 1.0d0
+       t = 0.0d0
+       open(file='posicao', unit=7)
+       xant = 0.0d0
+       yant = 1.0d0
+       y = yant + (v0*dt)
+       x = xant + (v0*dt)
+       
+       
+       
+       
+       do while (t.lt.tf)
+         r = sqrt((x**2.0d0)+(y**2.0d0))**3.0d0
          x = (2.0d0*x)-xant+((-1.0d0)*((GMs*x)/(r))*(dt**2.0d0))
-       end function
+         y = (2.0d0*y)-yant+((-1.0d0)*((GMs*y)/(r))*(dt**2.0d0))
 
-       real*8 function yi(m)
-         implicit real*8 (a-h, o-z)
-         common x, y, xant, yant, GMs
+         write(7,*)
+         t = t + dt
+       end do
 
-       end function
-
+       end
