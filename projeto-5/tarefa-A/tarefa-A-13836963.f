a2aa64ac249b1770b0e756061507281f05e5b8ed
Luana <luana@luana.dev.br>
2023-12-04 10:08:03 -0300

    a

---
+++ projeto-5/tarefa-A/tarefa-A-13836963.f
@@ -0,0 +1,19 @@
+       program palavra
+       implicit real*8 (a-h, o-z)
+       common x, y, xant, yant
+       m = 0
+
+       end program
+
+       real*8 function xi(m)
+         implicit real*8 (a-h, o-z)
+         common x, y, xant, yant
+         x = (2*x)-xant+()
+       end function
+
+       real*8 function yi(m)
+         implicit real*8 (a-h, o-z)
+         common x, y, xant, yant
+
+       end function
+
